9af11a3943bf0cfac737ebda59f4c9acdaec2b8c
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-21 10:01:32 +0200

    created controlled elements in Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
     console.log(e);
@@ -6,7 +10,11 @@ const Form = () => {
   return (
     <form className="bg-[#FFD79C] py-5" onSubmit={handleSubmit}>
       <h2 className="mb-2">What do you need for your trip?</h2>
-      <select className="mx-1 h-[36px] rounded-full px-5 py-1 text-lg font-semibold text-slate-700">
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+        className="mx-1 h-[36px] rounded-full px-5 py-1 text-lg font-semibold text-slate-700"
+      >
         {Array.from({ length: 20 }, (ele, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -15,6 +23,8 @@ const Form = () => {
       </select>
       <input
         type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
         placeholder="Item..."
         className="mx-1 rounded-full px-5 py-1 text-lg font-semibold text-slate-700"
       />
