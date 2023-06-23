697e9981678579823db2743107b8b0c61c81e982
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-23 12:27:43 +0200

    layout fixed

--- src/components/Form.js
+++ src/components/Form.js
@@ -22,28 +22,30 @@ const Form = ({ addItems }) => {
 
   return (
     <form className="bg-[#FFD79C] py-5" onSubmit={handleSubmit}>
-      <h2 className="mb-2">What do you need for your trip?</h2>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-        className="mx-1 h-[36px] rounded-full px-5 py-1 text-lg font-semibold text-slate-700"
-      >
-        {Array.from({ length: 20 }, (ele, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder="Item..."
-        className="mx-1 rounded-full px-5 py-1 text-lg font-semibold text-slate-700"
-      />
-      <button className="mx-1 rounded-full bg-amber-800 px-5 py-1 text-lg font-semibold text-slate-300 hover:bg-amber-700">
-        Add
-      </button>
+      <h2 className="">What do you need for your trip?</h2>
+      <div className="">
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+          className="mx-1 my-3 h-[36px] rounded-full px-5 py-1 text-lg font-semibold text-slate-700"
+        >
+          {Array.from({ length: 20 }, (ele, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Item..."
+          className="mx-1 rounded-full px-5 py-1 text-lg font-semibold text-slate-700"
+        />
+        <button className="mx-1 rounded-full bg-amber-800 px-5 py-1 text-lg font-semibold text-slate-300 hover:bg-amber-700">
+          Add
+        </button>
+      </div>
     </form>
   );
 };
