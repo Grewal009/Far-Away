afe771ebc574d84601302e0b26290993206872ba
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-26 10:51:29 +0200

    css edited in Stats and PackingList component

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -37,13 +37,13 @@ const ParckingList = ({ items, deleteItem, status }) => {
         <select
           value={sort}
           onChange={(e) => setSort(e.target.value)}
-          className="mx-1 w-auto rounded-full px-3 py-1 text-center text-sm font-semibold uppercase text-slate-700"
+          className=" w-auto rounded-full px-3 py-1 text-center text-sm font-semibold uppercase text-slate-700"
         >
           <option value="input">Sort by input order</option>
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
-        <button className="ml-2 w-auto rounded-full bg-amber-800 px-3 py-1 text-sm uppercase text-slate-300 hover:bg-amber-700">
+        <button className="ml-2 mt-2 w-auto rounded-full bg-amber-800 px-3 py-1 text-sm uppercase text-slate-300 hover:bg-amber-700">
           Clear
         </button>
       </div>
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,7 @@
 const Stats = ({ items }) => {
   if (!items.length)
     return (
-      <p className="bg-[#85A389] py-5">
+      <p className="flex h-[80px] items-center justify-center bg-[#85A389] px-2 sm:py-10">
         <em>Start adding some items to your packing list 🚀</em>
       </p>
     );
@@ -11,7 +11,7 @@ const Stats = ({ items }) => {
   const percentage = Math.round((packedItems / numItems) * 100);
 
   return (
-    <footer className="bg-[#85A389] py-5">
+    <footer className="flex h-[80px] items-center justify-center bg-[#85A389] px-2 sm:py-10">
       <em>
         {percentage === 100
           ? `You got everything! Ready to go 🚀`
